62a166e6c1b89b929f8e256d9a1adee67b38f4b8
Josh Buhler <josh@buhler.me>
2019-11-29 13:21:47 -0700

    Adding presenter w/ lots of placeholders

---
+++ SyncTunes/SyncTunes/SyncTunesPresenter.swift
@@ -0,0 +1,45 @@
+//
+//  SyncTunesPresenter.swift
+//  SyncTunes
+//
+//  Created by Joshua Buhler on 11/27/19.
+//  Copyright © 2019 buhler.me. All rights reserved.
+//
+
+import Foundation
+
+class SyncTunesPresenter {
+    
+    func addPlaylistFile (fileURL:URL) {
+        
+    }
+    
+    func buildTrackList () {
+        
+    }
+    
+    func trimPathAncestors () {
+        
+    }
+    
+    func clearDeletedTracks () {
+        
+    }
+    
+    func createOutputDir () {
+        
+    }
+    
+    func writePlaylistFiles () {
+        
+    }
+    
+    func copyTracksToOutputDir () {
+        
+    }
+    
+    func notifyUIThatWereDone () {
+        
+    }
+    
+}
--- SyncTunes/SyncTunesTests/SyncTunesPresenterTests.swift
+++ SyncTunes/SyncTunesTests/SyncTunesPresenterTests.swift
@@ -1,5 +1,5 @@
 //
-//  SyncTunesTests.swift
+//  SyncTunesPresenterTests.swift
 //  SyncTunesTests
 //
 //  Created by Joshua Buhler on 11/7/19.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import SyncTunes
 
-class SyncTunesTests: XCTestCase {
+class SyncTunesPresenterTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +19,9 @@ class SyncTunesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_init () {
+        let presenter = SyncTunesPresenter ()
+        XCTAssertNotNil(presenter)
     }
 
 }
